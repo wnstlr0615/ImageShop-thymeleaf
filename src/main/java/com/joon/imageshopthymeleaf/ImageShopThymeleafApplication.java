feb5b41504c9e331e1c9a9e4efc8067af2d762fa
wnstlr0615 <ryan0@kakao.com>
2021-06-22 10:30:22 +0900

    project set up

---
+++ src/main/java/com/joon/imageshopthymeleaf/ImageShopThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.joon.imageshopthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageShopThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageShopThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joon/imageshopthymeleaf/ImageShopThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joon.imageshopthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageShopThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
