169bbc9e943d8f69b93bb9456b5ca364defc1e88
wnstlr0615 <ryan0@kakao.com>
2021-06-22 11:32:12 +0900

    Messages.properties 메시기프로퍼티  추가

--- src/main/java/com/joon/imageshopthymeleaf/controller/HomeController.java
+++ src/main/java/com/joon/imageshopthymeleaf/controller/HomeController.java
@@ -1,17 +1,21 @@
 package com.joon.imageshopthymeleaf.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.DateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home( Model model){
         LocalDateTime now=LocalDateTime.now();
-        model.addAttribute("serverTime", now);
+        String serverTime = now.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        model.addAttribute("serverTime", serverTime);
         return "home";
     }
 }
